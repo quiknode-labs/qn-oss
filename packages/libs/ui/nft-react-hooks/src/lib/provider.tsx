a8c83b9a59a699906c7e6132ce8c95caaa158d6e
AlejandroFabianCampos <alejandro@quiknode.io>
2022-08-04 17:50:14 -0300

    chore: updated no api key messaging based on @gayleQN 's feedback

--- packages/libs/ui/nft-react-hooks/src/lib/provider.tsx
+++ packages/libs/ui/nft-react-hooks/src/lib/provider.tsx
@@ -74,7 +74,7 @@ function IcyProvider(props: IcyProviderProps) {
     const { apiKey } = props;
     if (typeof apiKey !== 'string' || apiKey.length === 0) {
       console.warn(
-        'nft-react-hooks warning: no apiKey provided, this is only intended for development. For production usage create an account on https://developers.icy.tools/'
+        'nft-react-hooks warning: no apiKey provided. Access with no apiKey is heavily rate limited and intended for development use only. For higher rate limits or production usage, create an account on https://developers.icy.tools/'
       );
     }
     tokenStore.setToken(apiKey ?? null);

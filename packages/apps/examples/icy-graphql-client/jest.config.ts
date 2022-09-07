7c0d45804b37d5823ba52b0a6ef2664ebbc27964
AlejandroFabianCampos <alejandro@quiknode.io>
2022-09-07 11:15:14 -0300

    chore(icy-graphql-client): adds example express app utilizing the graphql client

---
+++ packages/apps/examples/icy-graphql-client/jest.config.ts
@@ -0,0 +1,17 @@
+/* eslint-disable */
+export default {
+  displayName: 'apps-examples-icy-graphql-client',
+  preset: '../../../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory:
+    '../../../../coverage/packages/apps/examples/icy-graphql-client',
+};

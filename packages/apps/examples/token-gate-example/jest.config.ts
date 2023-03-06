7961962c960e211d8602d794814323a9912b732e
John Mitsch <john@quicknode.com>
2023-03-06 14:29:48 -0500

    inited token gate example

---
+++ packages/apps/examples/token-gate-example/jest.config.ts
@@ -0,0 +1,12 @@
+/* eslint-disable */
+export default {
+  displayName: 'apps-examples-token-gate-example',
+  preset: '../../../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory:
+    '../../../../coverage/packages/apps/examples/token-gate-example',
+};

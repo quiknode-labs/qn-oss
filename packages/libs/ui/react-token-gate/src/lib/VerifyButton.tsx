6f9c582db09b582daceaf90f7cbb716c05cb16a0
John Mitsch <john@quicknode.com>
2023-03-08 13:36:15 -0500

    Styled components added

--- packages/libs/ui/react-token-gate/src/lib/VerifyButton.tsx
+++ packages/libs/ui/react-token-gate/src/lib/VerifyButton.tsx
@@ -1,13 +1,23 @@
+import styled from 'styled-components';
 interface VerifyButtonProps {
   startVerification: () => void;
   buttonPrompt: string;
 }
 
 function VerifyButton({ startVerification, buttonPrompt }: VerifyButtonProps) {
+  const Button = styled.button`
+    background: transparent;
+    border-radius: 3px;
+    border: 2px solid palevioletred;
+    color: palevioletred;
+    margin: 0 1em;
+    padding: 0.25em 1em;
+  `;
+
   return (
-    <button className="p-4 text-3xl" onClick={startVerification}>
+    <Button className="" onClick={startVerification}>
       {buttonPrompt}
-    </button>
+    </Button>
   );
 }
 

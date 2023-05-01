2f61077d854a104bf4447e93e1adc3a0f9cc8a1f
John Mitsch <john@quicknode.com>
2023-05-01 12:09:20 -0400

    ran migrations

--- packages/apps/examples/sdk-api/jest.config.ts
+++ packages/apps/examples/sdk-api/jest.config.ts
@@ -2,14 +2,15 @@
 export default {
   displayName: 'apps-examples-sdk-api',
   preset: '../../../../jest.preset.js',
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-    },
-  },
+  globals: {},
   testEnvironment: 'node',
   transform: {
-    '^.+\\.[tj]s$': 'ts-jest',
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../../../coverage/packages/apps/examples/sdk-api',
--- tools/build-lib-nft-hooks/rollup.config.cjs
+++ tools/build-lib-nft-hooks/rollup.config.cjs
@@ -1,19 +1,18 @@
 // https://antonball.medium.com/extending-rollup-configuration-for-nx-a568cec16bf4
 
-const nrwlConfig = require('@nrwl/react/plugins/bundle-rollup')
+const nrwlConfig = require('@nx/react/plugins/bundle-rollup');
 
 module.exports = (config) => {
-    // console.log(`config = ${JSON.stringify(config, null, 2)}`);
-    const nxConfig = nrwlConfig(config);
-    // nxConfig.input = { input: nxConfig.input }
-    nxConfig.output.name = 'Icy NFT Hooks';
-    
-    const format = process.env['FORMAT'] ?? 'esm'
-    // console.log(`env format = ${format}; current format = ${nxConfig.output.format}`)
-    nxConfig.output.format = format
-    
-    // console.log(`nxConfig = ${JSON.stringify(nxConfig, null, 2)}`);
+  // console.log(`config = ${JSON.stringify(config, null, 2)}`);
+  const nxConfig = nrwlConfig(config);
+  // nxConfig.input = { input: nxConfig.input }
+  nxConfig.output.name = 'Icy NFT Hooks';
 
-    return nxConfig;
-}
+  const format = process.env['FORMAT'] ?? 'esm';
+  // console.log(`env format = ${format}; current format = ${nxConfig.output.format}`)
+  nxConfig.output.format = format;
 
+  // console.log(`nxConfig = ${JSON.stringify(nxConfig, null, 2)}`);
+
+  return nxConfig;
+};

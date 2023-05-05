5d7127599131b45fc6dd50d695769eac8ef88244
John Mitsch <john@quicknode.com>
2023-05-05 10:44:59 -0400

    WIP

--- packages/libs/sdk/spec/testSetup/jestSetupAfterEnv.ts
+++ packages/libs/sdk/spec/testSetup/jestSetupAfterEnv.ts
@@ -1,5 +1,7 @@
 // @ts-ignore
-beforeEach(async () => {
+afterEach(async () => {
   // pause between tests to avoid test race conditions
+  console.log('HERE1');
   await new Promise((resolve) => setTimeout(resolve, 1000));
+  console.log('HERE2');
 });

85824163968d7885bee4b33c2cf45b5c9f612888
John Mitsch <john@quicknode.com>
2023-05-17 09:26:35 -0400

    wip

--- packages/libs/sdk/rollup.esm.mjs
+++ packages/libs/sdk/rollup.esm.mjs
@@ -1,39 +1,43 @@
 import typescript from '@rollup/plugin-typescript';
 import copy from 'rollup-plugin-copy';
-import { terser } from 'rollup-plugin-terser';
+//import { terser } from 'rollup-plugin-terser';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 import path from 'path';
 import externals from 'rollup-plugin-node-externals';
 import { URL } from 'url';
+import commonjs from '@rollup/plugin-commonjs';
 
 // esm patch for __dirname
 const __dirname = new URL('.', import.meta.url).pathname;
 const rootDir = path.resolve(__dirname);
 const toAbsoluteDir = (relativeDir) => path.resolve(rootDir, relativeDir);
+const EXTERNALS = ['cross-fetch', 'graphql'];
 
 export default {
   input: toAbsoluteDir('./src/index.ts'),
   output: {
-    file: 'dist/packages/libs/sdk/src/index.esm.js',
+    file: 'dist/packages/libs/sdk/esm/index.js',
     format: 'esm',
     sourcemap: 'inline',
     sourcemapExcludeSources: true,
   },
   plugins: [
+    externals(), // Ignore all external dependencies and builtin modules
+    nodeResolve(),
+    commonjs(),
     typescript({
       tsconfig: toAbsoluteDir('tsconfig.esm.json'),
-      declaration: false, // using dts-bundle-generator CLI for declaration files
     }),
     copy({
       targets: [
         {
           src: [toAbsoluteDir('README.md'), toAbsoluteDir('package.json')],
-          dest: 'dist/packages/libs/sdk',
+          dest: 'dist/packages/libs/sdk/',
         },
       ],
     }),
     // Minify library code
-    terser(),
-    // Ignore all external dependencies and builtin modules
-    externals(),
+    //terser(),
   ],
+  externals: EXTERNALS,
 };

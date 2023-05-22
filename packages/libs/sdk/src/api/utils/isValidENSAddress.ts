1bc630d143bc577c40e0abf584fc82a9a3be9909
John Mitsch <john@quicknode.com>
2023-05-22 12:56:36 -0400

    tests passing

--- packages/libs/sdk/src/api/utils/isValidENSAddress.ts
+++ packages/libs/sdk/src/api/utils/isValidENSAddress.ts
@@ -1,9 +1,10 @@
 export function isValidENSAddress(ensAddress: string): boolean {
   const allowedTLDs = ['eth', 'xyz', 'art'];
-  const regexPattern = `^(?=.{3,255}$)\\p{L}+[\\p{L}\\p{N}\\p{M}]*\\.(?:${allowedTLDs.join(
+  const repeatedCharPattern =
+    '[\\p{L}\\p{N}\\p{Pd}\\p{M}\\u{1F300}-\\u{1F6FF}]';
+  const regexPattern = `^(?=.{3,255}$)${repeatedCharPattern}+(\\.${repeatedCharPattern}+)*\\.(?:${allowedTLDs.join(
     '|'
   )})$`;
-  console.log(regexPattern);
-  const regex = new RegExp(regexPattern, 'mui');
+  const regex = new RegExp(regexPattern, 'mu');
   return regex.test(ensAddress);
 }

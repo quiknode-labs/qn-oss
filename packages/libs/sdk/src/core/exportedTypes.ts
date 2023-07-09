85edad96a8c8c6b27ceda03eedfddc08fec92ce9
John Mitsch <john@quicknode.com>
2023-07-09 11:40:31 -0400

    Add more exported types

--- packages/libs/sdk/src/core/exportedTypes.ts
+++ packages/libs/sdk/src/core/exportedTypes.ts
@@ -6,4 +6,36 @@ export {
   QNFetchNFTCollectionDetailsInput,
   QNFetchNFTCollectionDetailsResponse,
 } from './addOns/nftTokenV2/types/qn_fetchNFTCollectionDetails';
+export {
+  QNFetchNFTsByCollectionInput,
+  QNFetchNFTsByCollectionResult,
+} from './addOns/nftTokenV2/types/qn_fetchNFTsByCollection';
+export {
+  QNGetTokenMetadataByCAInput,
+  QNGetTokenMetadataByCAResult,
+} from './addOns/nftTokenV2/types/qn_getTokenMetadataByContractAddress';
+export {
+  QNGetTokenMetadataBySymbolInput,
+  QNGetTokenMetadataBySymbolResult,
+} from './addOns/nftTokenV2/types/qn_getTokenMetadataBySymbol';
+export {
+  QNGetTransactionsByAddressInput,
+  QNGetTransactionsByAddressResult,
+} from './addOns/nftTokenV2/types/qn_getTransactionsByAddress';
+export {
+  QNGetTransfersByNFTInput,
+  QNGetTransfersByNFTResult,
+} from './addOns/nftTokenV2/types/qn_getTransfersByNFT';
+export {
+  QNGetWalletTokenBalanceInput,
+  QNGetWalletTokenBalanceResult,
+} from './addOns/nftTokenV2/types/qn_getWalletTokenBalance';
+export {
+  QNGetWalletTokenTransactionsInput,
+  QNGetWalletTokenTransactionsResult,
+} from './addOns/nftTokenV2/types/qn_getWalletTokenTransactions';
+export {
+  QNVerifyNFTsOwnerInput,
+  QNVerifyNFTsOwnerResult,
+} from './addOns/nftTokenV2/types/qn_verifyNFTsOwner';
 export { CoreArguments, QNCoreClientConfig, QNCoreClient } from './coreTypes';

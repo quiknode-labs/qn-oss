c7ea9f1a8f6b5df0bf0ad222ef0f2e95bb7f1efe
John Mitsch <john@quicknode.com>
2023-07-10 19:10:30 -0400

    Update env var

--- packages/libs/sdk/spec/core/client.ts
+++ packages/libs/sdk/spec/core/client.ts
@@ -2,9 +2,9 @@ import { Core, CoreArguments } from '../../src';
 
 const coreOpts: CoreArguments = {
   // This endpoint URL hostname has to match what is recorded locally with what is on CI. In github actions,
-  // we can put a redacted auth token since we filter it out with polly, for example:
-  // https://green-bitter-firefly.quiknode.pro/redacted/
-  endpointUrl: process.env['QUICKNODE_ENDPOINT_URL'] || '',
+  // we can put a invalidated auth token since we filter it out with polly.
+  endpointUrl:
+    process.env['QUICKNODE_ENDPOINT_URL'] || 'thisisnotanendpoint.example.com',
   config: {
     addOns: { nftTokenV2: true },
   },

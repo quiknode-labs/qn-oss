b4f486b046e9de6986743fe900f557473ac62ce9
John Mitsch <john@quicknode.com>
2023-07-11 19:12:03 -0400

    try adding globalThis fetch

--- packages/libs/sdk/src/core/core.ts
+++ packages/libs/sdk/src/core/core.ts
@@ -7,10 +7,13 @@ import {
   type QNCoreClient,
 } from './coreTypes';
 import { deriveNetworkFromUrl } from './networks';
-import fetch from 'cross-fetch';
+import fetch, { Headers, Request, Response } from 'cross-fetch';
 
-if (!globalThis.fetch && !process.env['CI']) {
+if (!globalThis.fetch) {
   globalThis.fetch = fetch;
+  globalThis.Headers = Headers;
+  globalThis.Request = Request;
+  globalThis.Response = Response;
 }
 
 export const buildQNActions = (config: QNCoreClientConfig) => {

9be03db956218ddd01e854f76ac41eb804357030
John Mitsch <john@quicknode.com>
2023-09-21 14:26:43 -0400

    use klona

--- packages/libs/sdk/src/api/graphql/modifyQueryForChain.ts
+++ packages/libs/sdk/src/api/graphql/modifyQueryForChain.ts
@@ -1,6 +1,7 @@
 import { DefinitionNode, FieldNode, Kind, DocumentNode } from 'graphql';
 import { TypedDocumentNode } from '@urql/core';
 import { ChainName } from '../types/chains';
+import { klona } from 'klona';
 
 type Mutable<T> = {
   -readonly [k in keyof T]: T[k];
@@ -8,41 +9,13 @@ type Mutable<T> = {
 
 type MutableDocumentNode = Mutable<DocumentNode>;
 
-function deepCopyDocumentNode(
-  docNode: MutableDocumentNode
-): MutableDocumentNode {
-  return {
-    kind: docNode.kind,
-    definitions: docNode.definitions.map((def) => {
-      if (def.kind === Kind.OPERATION_DEFINITION) {
-        return {
-          ...def,
-          selectionSet: {
-            ...def.selectionSet,
-            selections: def.selectionSet.selections.map((selection) => {
-              if (selection.kind === Kind.FIELD) {
-                return {
-                  ...selection,
-                  name: { ...selection.name },
-                };
-              }
-              return selection;
-            }),
-          },
-        };
-      }
-      return def;
-    }),
-  };
-}
-
 export function modifyQueryForChain<TQuery, TQueryVariables>(
   chainName: ChainName,
   originalDocumentNode: MutableDocumentNode
 ): TypedDocumentNode<TQuery, TQueryVariables> {
   // We need to deep clone the document node in order to not mutate the query so it is consistent
   // across multiple calls to the same query with different chains
-  const documentNode = deepCopyDocumentNode(originalDocumentNode);
+  const documentNode = klona(originalDocumentNode);
 
   documentNode.definitions = documentNode.definitions.map(
     (doc: DefinitionNode) => {

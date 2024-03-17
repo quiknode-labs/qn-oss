7c09b06e83dc7f445a5f5ddff17f9776ec9fc264
John Mitsch <john@quicknode.com>
2024-03-17 20:18:20 -0300

    wip

--- packages/libs/sdk/spec/solana/client.ts
+++ packages/libs/sdk/spec/solana/client.ts
@@ -3,7 +3,7 @@ import { Solana, SolanaClientArgs } from '../../src';
 const solanaOpts: SolanaClientArgs = {
   endpointUrl:
     process.env['QUICKNODE_SOLANA_ENDPOINT_URL'] ||
-    'https://thisisnotanendpoint.example.com',
+    'https://thisisnotasolanaendpoint.example.com',
 };
 
 export const solana = new Solana(solanaOpts);
